f1acfbc1fd380c3904157f4b13b5cbb802b461cc
James Colestock <james@colestock.com>
2018-10-20 01:03:12 -0600

    Update db.js
    Formatting db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const Promise = require('bluebird'); 
+const Promise = require('bluebird');
 Promise.promisifyAll(mysql);
 Promise.promisifyAll(require("mysql/lib/Connection").prototype);
 Promise.promisifyAll(require("mysql/lib/Pool").prototype);
@@ -15,20 +15,22 @@ const dbconfig = {
 
 let pool = Promise.promisifyAll(mysql.createPool(dbconfig));
 
-pool.on('acquire', (connection) => { 
-  console.log('connection %d', connection.threadId); 
-}); 
+pool.on('acquire', (connection) => {
+  console.log('connection %d', connection.threadId);
+});
 
 const getConnection = exports.getConnection = () => {
   return pool.getConnectionAsync().disposer((connection) => {
     try {
       connection.release();
-    } catch(e) {};
+    } catch (e) {};
   });
 }
 
 const query = exports.query = (sql, placeholders) => {
-   return Promise.using(getConnection(), (connection) => {
-      return Promise.promisify(connection.query, {context: connection})(sql, placeholders);
-   });
+  return Promise.using(getConnection(), (connection) => {
+    return Promise.promisify(connection.query, {
+      context: connection
+    })(sql, placeholders);
+  });
 }
